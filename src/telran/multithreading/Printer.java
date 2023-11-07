499e0368dd84a55e2cc5add860ed920f58b34609
Yuragranovsky <yuragranovsky@gmail.com>
2023-11-07 17:51:12 +0200

    HW #42 scheduled printing

--- src/telran/multithreading/Printer.java
+++ src/telran/multithreading/Printer.java
@@ -1,21 +1,45 @@
 package telran.multithreading;
 
 public class Printer extends Thread {
-	private char symbol;
-	private int nRuns;
-	public Printer(char symbol, int nRuns) {
-		this.symbol = symbol;
-		this.nRuns = nRuns;
+	int printerNumber;
+	Printer next;
+	private int inLine;
+	static int overall;
+	static int nPartitions;
+
+	public Printer(int printerNumber) {
+		this.printerNumber = printerNumber;
+		inLine = overall / nPartitions;
 	}
+
+	public void setNext(Printer next) {
+		this.next = next;
+	}
+
+	public static void setOverall(int overall) {
+		Printer.overall = overall;
+	}
+
+	public static void setPartitions(int nPartitions) {
+		Printer.nPartitions = nPartitions;
+
+	}
+
 	@Override
 	public void run() {
-		for(int i = 0; i < nRuns; i++) {
+		int count = 0;
+		String line = (""+ printerNumber).repeat(inLine);
+		while (count < nPartitions) {
 			try {
-				sleep(10);
+				join();
+
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				System.out.println(line);
+				next.interrupt();
+				count++;
 			}
-			System.out.println(symbol);
 		}
+
 	}
+
 }

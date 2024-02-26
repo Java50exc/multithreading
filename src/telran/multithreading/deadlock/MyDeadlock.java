70787b4f2b05db73dc68a97d7d2b60a2bcd98097
allanteone <allanteone@gmail.com>
2024-02-26 17:23:14 +0200

    hw_44 deadlock

---
+++ src/telran/multithreading/deadlock/MyDeadlock.java
@@ -0,0 +1,34 @@
+package telran.multithreading.deadlock;
+
+public class MyDeadlock {
+	
+	public static void main(String[] args) throws InterruptedException {
+		Object i1 = new Object();
+		Object i2 = new Object();
+		
+		Thread t1 = new Thread(() -> {	
+			synchronized (i1) {	
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {}
+				synchronized (i2) {}
+				
+			}
+		});
+		
+		Thread t2 = new Thread(() -> {
+			synchronized (i2) {	
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {}
+				synchronized (i1) {}	
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		t1.join();
+		
+	}
+
+}
